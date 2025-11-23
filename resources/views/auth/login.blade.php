4137b840b8b586d2ee7bc01d1af2242c1cc3985f
Rolando-web <r.luayon.548817@umindanao.edu.ph>
2025-11-23 17:35:20 +0000

    csrf removed form login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,6 @@
           @endif
 
           <form class="space-y-4" method="POST" action="{{ route('login') }}">
-              @csrf
             <div>
               <label for="email" class="block text-sm font-medium  text-left">Email</label>
               <input required type="email" placeholder="Email" id="email" name="email" value="{{ old('email') }}" class="text-black mt-1 w-full px-3 py-2  border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
